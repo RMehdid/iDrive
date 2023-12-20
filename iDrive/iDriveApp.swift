19e35d77b482b1d53d0d4f77500da0f18e31d30e
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-12-20 21:55:44 +0100

    Initial Commit

---
+++ iDrive/iDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDriveApp.swift
+//  iDrive
+//
+//  Created by Samy Mehdid on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
